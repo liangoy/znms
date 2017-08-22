30d79ceb645229da3d429623c8ba211723e43e9f
liangoy <emailboxbeta@gmail.com>
2017-08-22 15:25:54 +0800

    Update fn.py

--- fn.py
+++ fn.py
@@ -113,7 +113,7 @@ def get_news(value):#http://139.196.88.54:6677/?action=fn.get_news&value={%22nam
     return lis
 
 def if_none(value):
-    return  [[good_stock.__name__, good_stock('推荐')  ]]
+    return  [['text':'主人，您的话太深奥了，我给您推荐一只股票吧！'],[good_stock.__name__, good_stock('推荐')  ]]
     
     
     
