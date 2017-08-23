1dee5e3a90eee4e19ea5d00303c3ccbec3ba5993
liangoy <emailboxbeta@gmail.com>
2017-08-23 17:52:13 +0800

    Update fn.py

--- fn.py
+++ fn.py
@@ -1,79 +1,65 @@
 from w2v import W2v
 import json
 import mem_data_client
-w2v=W2v('./w2v.json','./stock_fun_list','./jieba_stock_fun_dict')
-with open('./stock_fun_code') as f:stock_fun_code=json.loads(f.read())
-
+w2v=W2v('./w2v.json')
+with open('./stock_fun_code') as f:stock_fun=json.loads(f.read())
+w2v.name=list(map(lambda x:x['name'],stock_fun))
+w2v.jieba_loads_stock_fun()
 #precedence_table
 pt={}
 
-def n2c(name):
-    return list(filter(lambda x:x['name']==name,stock_fun_code))[0]
+stock_list=list(map(lambda x:x['name'],filter(lambda x:x['type']=='stock' ,stock_fun)))
+fun_list=list(map(lambda x:x['name'],filter(lambda x:x['type']=='fun' ,stock_fun)))
 
-def stock_info(t):
-    lis=[]
-    for i in w2v.name:
-        if i in  t:
-            lis.append(n2c(i))
+def n2c(name):
+    return list(filter(lambda x:x['name']==name,stock_fun))[0]
+
+def stock_fun_info(t):
+    lis=list(map(lambda x:n2c(x) ,filter(lambda x:x in t,w2v.name)))
     if len(lis)==1:
         return lis[0]
     else :
         return None
-pt[2]=stock_info
+pt[2]=stock_fun_info
 
 def stock_vs(t):
-    lis=[]
-    for i in w2v.name:
-        if i in t:
-            lis.append(n2c(i))
+    lis=list(map(lambda x:n2c(x)['code'] ,filter(lambda x:x in t,stock_list)))
     if len(lis)>=2:
-        return lis
+        return {'code':lis}
     else:
         return None
 pt[3]=stock_vs
 
-def stock_news(t):
-    lis=[]
-    for i in w2v.name:
-        if i in t:
-            lis.append(n2c(i))
-    if len(lis)==1:
-        if '资讯'in t or '新闻' in t:
-            return lis[0]
+def stock_fun_news(t):
+    lis=list(map(lambda x:n2c(x) ,filter(lambda x:x in t,w2v.name)))
+    if len(lis)==1 and ('资讯'in t or '新闻' in t):
+        return lis[0]
     return None
-pt[4]=stock_news
+pt[4]=stock_fun_news
 
 def stock_stru(t):
-    lis=[]
-    for i in w2v.name:
-        if i in t:
-            lis.append(n2c(i))
-    if len(lis)==1:
-        if '股权结构'in t or '股东'in t:
-            return lis[0]
+    lis=list(map(lambda x:n2c(x) ,filter(lambda x:x in t,stock_list)))
+    if len(lis)==1 and ('股权结构'in t or '股东'in t):
+        return lis[0]
     return None
 pt[5]=stock_stru
 
 def stock_bsp(t):
-    lis=[]
-    for i in w2v.name:
-        if i in t:
-            lis.append(n2c(i))
-    if len(lis)==1:
-        if '买卖点' in t:
-            return lis[0]
+    lis=list(map(lambda x:n2c(x) ,filter(lambda x:x in t,stock_list)))
+    if len(lis)==1 and ('买卖点' in t):
+        return lis[0]
     return None
 pt[5.1]=stock_bsp
 
 def good_stock(t):
     if '推荐'in t:
-        return 'recommend'
+        return {}
     return None
 pt[6]=good_stock
 
 def index(t):
     if '大盘' in t:
-        return 'index'
+        return {}
     return None
 pt[7]=index
 
@@ -87,8 +73,9 @@ def translate(value):#http://139.196.88.54:6677/?action=fn.translate&value={%22t
     lis=[]
     for i in sorted(list(pt.keys()),reverse=1):
         resu=pt[i](text)
-        if resu:
-            lis.append([pt[i].__name__,resu])
+        if resu !=None:
+            resu['action']=pt[i].__name__
+            lis.append(resu)
     return lis[:top]
 
 
@@ -98,7 +85,7 @@ def get_news(value):#http://139.196.88.54:6677/?action=fn.get_news&value={%22nam
     top=str(value.get('top',3))
     name=value.get('name','')
     if code!='':
-        name=list(filter(lambda x:x['code']==code,stock_fun_code))[0]['name'] 
+        name=list(filter(lambda x:x['code']==code,stock_fun))[0]['name'] 
     #data=md.get("sorted(var['news'].map(lambda x:[x['score'][r'%s'],x['title'],x['time'],x['url']]),key=lambda x:x[0])[:%s]"%(name,top))['data']
     data=md.get("table['news'].filter(lambda x:r'%s' in x['content']).map(lambda x:[x['score'][r'%s'],x['title'],x['time'],x['url']])[:%s]"%(name,name,top))['data']
     lis=[]
@@ -113,24 +100,14 @@ def get_news(value):#http://139.196.88.54:6677/?action=fn.get_news&value={%22nam
     return lis
 
 def if_error(value):
-    reason=value.get('reason','')
-    if reason=='source can not find':
+    reason=int(value.get('reason',''))
+    if reason==101:
         text='不好意思哦，您要的资源暂时找不到'
-        return [['text',text]]
+        return [{'action':'text','text':text}]
     else:
-        text='您的话太深奥了，我先给您推荐股票吧！'
-        return  [['text',text],[good_stock.__name__, good_stock('推荐')  ]]
+        text='您的话有点深奥哦，我先给您推荐股票吧！'
+        d=good_stock('推荐')
+        d['action']=good_stock.__name__
+        return  [{'action':'text','text':text},d]
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-       
