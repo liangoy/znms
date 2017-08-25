42c57f3002dd5b7ef28d021b3a18956ce6b24942
liangoy <emailboxbeta@gmail.com>
2017-08-25 09:52:36 +0800

    Update fn.py

--- fn.py
+++ fn.py
@@ -23,15 +23,15 @@ pt={}
 stock_list=list(map(lambda x:x['name'],filter(lambda x:x['type']=='stock' ,stock_fun)))
 fun_list=list(map(lambda x:x['name'],filter(lambda x:x['type']=='fun' ,stock_fun)))
 
-@add_action
 def n2c(name):
     return list(filter(lambda x:x['name']==name,stock_fun))[0]
+
 
 @add_action
 def stock_fun_info(t):
     lis=list(map(lambda x:n2c(x) ,filter(lambda x:x in t,w2v.name)))
     if len(lis)==1:
-        return lis[0]
+        return lis[0].copy()
     else :
         return None
 pt[2]=stock_fun_info
@@ -49,7 +49,7 @@ pt[3]=stock_vs
 def stock_fun_news(t):
     lis=list(map(lambda x:n2c(x) ,filter(lambda x:x in t,w2v.name)))
     if len(lis)==1 and ('资讯'in t or '新闻' in t):
-        return lis[0]
+        return lis[0].copy()
     return None
 pt[4]=stock_fun_news
 
@@ -57,7 +57,7 @@ pt[4]=stock_fun_news
 def stock_stru(t):
     lis=list(map(lambda x:n2c(x) ,filter(lambda x:x in t,stock_list)))
     if len(lis)==1 and ('股权结构'in t or '股东'in t):
-        return lis[0]
+        return lis[0].copy()
     return None
 pt[5]=stock_stru
 
@@ -65,7 +65,7 @@ pt[5]=stock_stru
 def stock_bsp(t):
     lis=list(map(lambda x:n2c(x) ,filter(lambda x:x in t,stock_list)))
     if len(lis)==1 and ('买卖点' in t):
-        return lis[0]
+        return lis[0].copy()
     return None
 pt[5.1]=stock_bsp
 
@@ -89,30 +89,24 @@ pt[7]=index
 
 def translate(value):#http://139.196.88.54:6677/?action=fn.translate&value={%22text%22:%22%E5%B7%A5%E5%95%86%E9%93%B6%E8%A1%8C%22,%22top%22:9}
     text=value['text']
-    top=value.get('top',3)
-    lis=list(filter(lambda x:x,map(lambda x:pt[x](text).copy(),sorted(list(pt.keys()),reverse=1))))
+    top=int(value.get('top',3))
+    lis=list(filter(lambda x:x,map(lambda x:pt[x](text),sorted(list(pt.keys()),reverse=1))))
+    if lis==[]:
+        return if_error({'reason':100})
     return lis[:top]
 
 
 md= mem_data_client.Client('http://139.196.88.54:1320')
 def get_news(value):#http://139.196.88.54:6677/?action=fn.get_news&value={%22name%22:%22%E5%B7%A5%E5%95%86%E9%93%B6%E8%A1%8C%22,%22top%22:9}
     code=value.get('code','')
-    top=str(value.get('top',3))
+    top=int(value.get('top',3))
     name=value.get('name','')
     if code!='':
         name=list(filter(lambda x:x['code']==code,stock_fun))[0]['name'] 
     #data=md.get("sorted(var['news'].map(lambda x:[x['score'][r'%s'],x['title'],x['time'],x['url']]),key=lambda x:x[0])[:%s]"%(name,top))['data']
     data=md.get("table['news'].filter(lambda x:r'%s' in x['content']).map(lambda x:[x['score'][r'%s'],x['title'],x['time'],x['url']])[:%s]"%(name,name,top))['data']
-    lis=[]
-    for i in data:
-        d={
-        'score':i[0],
-        'title':i[1],
-        'time':i[2],
-        'url':i[3]
-        }
-        lis.append(d)
-    return lis
+    lis=list(map(lambda i:{'score':i[0],'title':i[1],'time':i[2],'url':i[3]},data))
+    return lis[:top]
 
 def if_error(value):
     reason=int(value.get('reason',''))
