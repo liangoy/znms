ca1f68b7dbd075853fca43e1deb843c9c632bd42
liangoy <emailboxbeta@gmail.com>
2017-08-30 11:06:03 +0800

    Update fn.py

--- fn.py
+++ fn.py
@@ -125,7 +125,7 @@ def get_news(value):#http://139.196.88.54:6677/?action=fn.get_news&value={%22nam
     return lis[:top]
 
 def if_error(value):
-    if not value.get('_id'):
+    if not value.get('_id') and value.get('reason',100000)>99:
         raise "ERROR:YOU SHOULD POST _id"
     reason=int(value.get('reason',''))
     if reason==101:
